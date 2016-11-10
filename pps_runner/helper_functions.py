6869955b56ddcf59471d298273afd77c31a3f21e
Panu Lahtinen <pnuu+git@iki.fi>
2016-11-10 12:42:57 +0200

    Initial commit

---
+++ pps_runner/helper_functions.py
@@ -0,0 +1,41 @@
+from urlparse import urlparse, urlunsplit
+import socket
+import netifaces
+import os
+import logging
+
+
+def get_local_ips():
+    inet_addrs = [netifaces.ifaddresses(iface).get(netifaces.AF_INET)
+                  for iface in netifaces.interfaces()]
+    ips = []
+    for addr in inet_addrs:
+        if addr is not None:
+            for add in addr:
+                ips.append(add['addr'])
+    return ips
+
+
+def check_uri(uri):
+    """Check that the provided *uri* is on the local host and return the
+    file path.
+    """
+    if isinstance(uri, (list, set, tuple)):
+        paths = [check_uri(ressource) for ressource in uri]
+        return paths
+    url = urlparse(uri)
+    try:
+        if url.hostname:
+            url_ip = socket.gethostbyname(url.hostname)
+
+            if url_ip not in get_local_ips():
+                try:
+                    os.stat(url.path)
+                except OSError:
+                    raise IOError(
+                        "Data file %s unaccessible from this host" % uri)
+
+    except socket.gaierror:
+        logging.warning("Couldn't check file location, running anyway")
+
+    return url.path
---
+++ setup.py
@@ -0,0 +1,50 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+# Copyright (c) 2013, 2014, 2015, 2016 Martin Raspaud
+
+# Author(s):
+
+#   Martin Raspaud <martin.raspaud@smhi.se>
+
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Setup for pps-runner.
+"""
+from setuptools import setup
+import imp
+
+version = imp.load_source('pps_runner.version', 'pps_runner/version.py')
+
+setup(name="pps_runner",
+      version=version.__version__,
+      description='Pytroll runner for PPS',
+      author='Adam Dybroe',
+      author_email='adam.dybroe@smhi.se',
+      classifiers=["Development Status :: 3 - Alpha",
+                   "Intended Audience :: Science/Research",
+                   "License :: OSI Approved :: GNU General Public License v3 " +
+                   "or later (GPLv3+)",
+                   "Operating System :: OS Independent",
+                   "Programming Language :: Python",
+                   "Topic :: Scientific/Engineering"],
+      url="https://github.com/pytroll/pytroll-pps-runner",
+      packages=['pps_runner', ],
+      scripts=['bin/pps_runner.py', 'bin/pps_run.sh', ],
+      data_files=[],
+      zip_safe=False,
+      install_requires=['posttroll', ],
+      # test_requires=['mock'],
+      # test_suite='pps_runner.tests.suite',
+      )
