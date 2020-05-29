9a8ef0607be36a729695e1572a838b3eaf6a5299
Erik Johansson <erik.johansson@smhi.se>
2020-05-29 10:52:47 +0200

    initiate unitest for utils
    Signed-off-by: Erik Johansson <erik.johansson@smhi.se>

---
+++ nwcsafpps_runner/tests/test_utils.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+#
+# Copyright (c) 2020 pps2018_runner developers
+#
+# Author(s):
+#
+#   Erik Johansson <erik.johansson@smhi.se>
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+
+"""Test utility functions."""
+
+from posttroll.message import Message
+from trollmoves.utils import (get_outputfiles)
+# from trollmoves.utils import (get_outputfiles)
+import unittest
+import os
+import copy
+import datetime
+import unittest
+
+
+class Test(unittest.TestCase):
+
+
+    def testName(self):
+        pass
+
+
+if __name__ == "__main__":
+    #import sys;sys.argv = ['', 'Test.testName']
+    unittest.main()
