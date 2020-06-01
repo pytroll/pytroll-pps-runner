0bcdc50d312f847ae5cfe2538b3dc0b7d01b58d8
Erik Johansson <erik.johansson@smhi.se>
2020-06-01 11:13:40 +0200

    Bugfix, femoved brackets in import
    Signed-off-by: Erik Johansson <erik.johansson@smhi.se>

--- nwcsafpps_runner/tests/test_utils.py
+++ nwcsafpps_runner/tests/test_utils.py
@@ -21,7 +21,7 @@
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 """Test utility functions."""
-from nwcsafpps_runner.utils import (get_outputfiles)
+from nwcsafpps_runner.utils import get_outputfiles
 import os
 
 
