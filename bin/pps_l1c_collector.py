4330e3b0b8e8509ae299460c3949c604e0bb07f4
Adam Dybbroe <adybbroe@users.noreply.github.com>
2025-05-08 14:50:40 +0200

    Update bin/pps_l1c_collector.py

--- bin/pps_l1c_collector.py
+++ bin/pps_l1c_collector.py
@@ -3,11 +3,6 @@
 
 # Copyright (c) 2019 - 2021 Pytroll
 
-# Author(s):
-
-#   Erik Johansson <Firstname.Lastname at smhi.se>
-#   Adam Dybbroe <Firstname.Lastname at smhi.se>
-
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
